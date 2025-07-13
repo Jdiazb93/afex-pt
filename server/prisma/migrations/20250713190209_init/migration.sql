0f1069d331027e4ce3958b595dcf09702dd90d74
Jonathan Diaz Bustos <j.diaz.b93@hotmail.com>
2025-07-13 16:43:56 -0400

    Se agrega migrations folder

---
+++ server/prisma/migrations/20250713190209_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" DATETIME NOT NULL,
+    "name" TEXT NOT NULL,
+    "surname" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
+    "country" TEXT NOT NULL,
+    "agentType" TEXT NOT NULL,
+    "status" TEXT NOT NULL
+);
